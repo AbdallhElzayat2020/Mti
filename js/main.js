e5f0d5209be743cd93646a57bea5e55e8503a9d8
AbdallhElzayat2020 <https://github.com/AbdallhElzayat2020/E-commerce-React-App>
2023-07-01 03:38:58 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// ScrollReveal({
+//     reset: true
+// });
+
+// ScrollReveal({
+//     reset: true,
+//     distance: '80px',
+//     duration: 2000,
+//     delay: 200,
+// });
+
+// ScrollReveal().reveal('.logo', {
+//     origin: "top"
+// });
+
+
+// login
+document.getElementById("login-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username === "admin" && password === "password") {
+        document.getElementById("message").innerHTML = "تم تسجيل الدخول بنجاح!";
+        window.location.href = "mti-main/login.html";
+    } else {
+        document.getElementById("message").innerHTML = "اسم المستخدم أو كلمة المرور غير صحيحة!";
+    }
+});
